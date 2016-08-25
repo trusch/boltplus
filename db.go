b0cf9507397996653409f6108a092cbcbd91184e
Tino Rusch <tino.rusch@webvariants.de>
2016-08-25 09:27:22 +0200

    restored transaction interface;

--- db.go
+++ db.go
@@ -1,16 +1,6 @@
 package boltplus
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"log"
-	"strings"
-
-	"github.com/boltdb/bolt"
-	"github.com/golang/snappy"
-	"github.com/nytlabs/gojee"
-)
+import "github.com/boltdb/bolt"
 
 // DB wraps the boltdb handle
 type DB struct {
@@ -29,6 +19,15 @@ func New(filename string) (*DB, error) {
 	return db, db.open(filename)
 }
 
+// Tx creates a new transaction. Do not forget to commit all writing transactions and to close read and write messages!
+func (db *DB) Tx(writable bool) (*Transaction, error) {
+	tx, err := db.db.Begin(writable)
+	if err != nil {
+		return nil, err
+	}
+	return &Transaction{tx: tx}, nil
+}
+
 // Close closes the db
 func (db *DB) Close() {
 	db.db.Close()
@@ -36,191 +35,94 @@ func (db *DB) Close() {
 
 // Put inserts a doc into a bucket
 func (db *DB) Put(bucketPath, key string, val map[string]interface{}) error {
-	if err := db.db.Update(func(tx *bolt.Tx) error {
-		bucket, err := db.getBucketOrCreate(tx, bucketPath)
-		if err != nil {
-			log.Print("bucket err:", err)
-			return err
-		}
-		bs, err := db.dataToBytes(val)
-		if err != nil {
-			return err
-		}
-		if err := bucket.Put([]byte(key), bs); err != nil {
-			log.Print("put err:", err)
-			return err
-		}
-		return nil
-	}); err != nil {
+	tx, err := db.Tx(true)
+	if err != nil {
 		return err
 	}
-	return nil
+	defer tx.Close()
+	err = tx.Put(bucketPath, key, val)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
 }
 
 // Get retrieves a doc from a bucket
 func (db *DB) Get(bucketPath, key string) (map[string]interface{}, error) {
-	resultChan := make(chan map[string]interface{}, 1)
-	if err := db.db.View(func(tx *bolt.Tx) error {
-		bucket, err := db.getBucket(tx, bucketPath)
-		if err != nil {
-			return err
-		}
-		data := bucket.Get([]byte(key))
-		value, err := db.bytesToData(data)
-		if err != nil {
-			return err
-		}
-		resultChan <- value
-		return nil
-	}); err != nil {
+	tx, err := db.Tx(false)
+	if err != nil {
 		return nil, err
 	}
-	return <-resultChan, nil
+	defer tx.Close()
+	return tx.Get(bucketPath, key)
 }
 
 // Delete deletes a doc from a bucket
 func (db *DB) Delete(bucketPath, key string) error {
-	if err := db.db.Update(func(tx *bolt.Tx) error {
-		bucket, err := db.getBucket(tx, bucketPath)
-		if err != nil {
-			return err
-		}
-		if err := bucket.Delete([]byte(key)); err != nil {
-			return err
-		}
-		return nil
-	}); err != nil {
+	tx, err := db.Tx(true)
+	if err != nil {
 		return err
 	}
-	return nil
+	defer tx.Close()
+	err = tx.Delete(bucketPath, key)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
 }
 
 // GetBucketContent returns all docs in a bucket
 func (db *DB) GetBucketContent(bucketPath string) (chan *Pair, error) {
-	returnChannel := make(chan *Pair, 64)
-
-	tx, err := db.db.Begin(false)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-
-	bucket, err := db.getBucket(tx, bucketPath)
-	if err != nil {
-		return nil, err
-	}
-
-	go func() {
-		defer close(returnChannel)
-		defer tx.Rollback()
-		e := bucket.ForEach(func(k, v []byte) error {
-			if v != nil {
-				value, e := db.bytesToData(v)
-				if e != nil {
-					return e
-				}
-				returnChannel <- &Pair{string(k), value}
-			}
-			return nil
-		})
-		if e != nil {
-			log.Print(e)
-		}
-	}()
-
-	return returnChannel, nil
+	return tx.GetBucketContent(bucketPath)
 }
 
 // GetPrefix returns all docs in a bucket matching a prefix
 func (db *DB) GetPrefix(bucketPath, prefix string) (chan *Pair, error) {
-	returnChannel := make(chan *Pair, 64)
-
-	tx, err := db.db.Begin(false)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-
-	bucket, err := db.getBucket(tx, bucketPath)
-	if err != nil {
-		return nil, err
-	}
-
-	go func() {
-		defer close(returnChannel)
-		defer tx.Rollback()
-		c := bucket.Cursor()
-		for k, v := c.Seek([]byte(prefix)); bytes.HasPrefix(k, []byte(prefix)); k, v = c.Next() {
-			if v != nil {
-				value, e := db.bytesToData(v)
-				if e != nil {
-					log.Print(e)
-					continue
-				}
-				returnChannel <- &Pair{string(k), value}
-			}
-		}
-	}()
-
-	return returnChannel, nil
+	return tx.GetPrefix(bucketPath, prefix)
 }
 
 // GetRange returns all docs in a bucket matching a prefix
 func (db *DB) GetRange(bucketPath, start, end string) (chan *Pair, error) {
-	returnChannel := make(chan *Pair, 64)
-
-	tx, err := db.db.Begin(false)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-
-	bucket, err := db.getBucket(tx, bucketPath)
-	if err != nil {
-		return nil, err
-	}
-
-	go func() {
-		defer close(returnChannel)
-		defer tx.Rollback()
-		c := bucket.Cursor()
-		for k, v := c.Seek([]byte(start)); k != nil && bytes.Compare(k, []byte(end)) <= 0; k, v = c.Next() {
-			if v != nil {
-				value, e := db.bytesToData(v)
-				if e != nil {
-					log.Print(e)
-					continue
-				}
-				returnChannel <- &Pair{string(k), value}
-			}
-		}
-	}()
-
-	return returnChannel, nil
+	return tx.GetRange(bucketPath, start, end)
 }
 
 // Find searches a bucket for documents
 func (db *DB) Find(bucketPath, filterExpression string) (chan *Pair, error) {
-	stream, err := db.GetBucketContent(bucketPath)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-	return db.findFromStream(stream, filterExpression)
+	return tx.Find(bucketPath, filterExpression)
 }
 
 // FindPrefix searches a bucket for documents
 func (db *DB) FindPrefix(bucketPath, prefix, filterExpression string) (chan *Pair, error) {
-	stream, err := db.GetPrefix(bucketPath, prefix)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-	return db.findFromStream(stream, filterExpression)
+	return tx.FindPrefix(bucketPath, prefix, filterExpression)
 }
 
 // FindRange searches a bucket for documents
 func (db *DB) FindRange(bucketPath, start, end, filterExpression string) (chan *Pair, error) {
-	stream, err := db.GetRange(bucketPath, start, end)
+	tx, err := db.Tx(false)
 	if err != nil {
 		return nil, err
 	}
-	return db.findFromStream(stream, filterExpression)
+	return tx.FindRange(bucketPath, start, end, filterExpression)
 }
 
 func (db *DB) open(filename string) error {
@@ -230,83 +132,4 @@ func (db *DB) open(filename string) error {
 	}
 	db.db = dbHandle
 	return nil
-}
-
-func (db *DB) getBucketOrCreate(tx *bolt.Tx, bucketPath string) (*bolt.Bucket, error) {
-	buckets := strings.Split(bucketPath, ".")
-	bucket, err := tx.CreateBucketIfNotExists([]byte(buckets[0]))
-	if err != nil {
-		return nil, err
-	}
-	if len(bucketPath) > 1 {
-		for _, id := range buckets {
-			bucket, err = bucket.CreateBucketIfNotExists([]byte(id))
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return bucket, nil
-}
-
-func (db *DB) getBucket(tx *bolt.Tx, bucketPath string) (*bolt.Bucket, error) {
-	buckets := strings.Split(bucketPath, ".")
-	bucket := tx.Bucket([]byte(buckets[0]))
-	if bucket == nil {
-		return nil, errors.New("no such bucket")
-	}
-	if len(bucketPath) > 1 {
-		for _, id := range buckets {
-			bucket = bucket.Bucket([]byte(id))
-			if bucket == nil {
-				return nil, errors.New("no such bucket")
-			}
-		}
-	}
-	return bucket, nil
-}
-
-func (db *DB) dataToBytes(data map[string]interface{}) ([]byte, error) {
-	var buff bytes.Buffer
-	encoder := json.NewEncoder(snappy.NewWriter(&buff))
-	if err := encoder.Encode(data); err != nil {
-		return nil, err
-	}
-	return buff.Bytes(), nil
-}
-
-func (db *DB) bytesToData(data []byte) (map[string]interface{}, error) {
-	buff := bytes.NewBuffer(data)
-	decoder := json.NewDecoder(snappy.NewReader(buff))
-	value := make(map[string]interface{})
-	err := decoder.Decode(&value)
-	if err != nil {
-		return nil, err
-	}
-	return value, nil
-}
-
-func (db *DB) findFromStream(stream chan *Pair, filterExpression string) (chan *Pair, error) {
-	res := make(chan *Pair, 64)
-	tokens, err := jee.Lexer(filterExpression)
-	if err != nil {
-		return nil, err
-	}
-	tree, err := jee.Parser(tokens)
-	if err != nil {
-		return nil, err
-	}
-	go func() {
-		defer close(res)
-		for pair := range stream {
-			if val, err := jee.Eval(tree, pair.Value); err == nil {
-				if match, ok := val.(bool); ok && match {
-					res <- pair
-				}
-			} else {
-				log.Print(err)
-			}
-		}
-	}()
-	return res, nil
 }
---
+++ tx.go
@@ -0,0 +1,272 @@
+package boltplus
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/boltdb/bolt"
+	"github.com/golang/snappy"
+	"github.com/nytlabs/gojee"
+)
+
+// Transaction represents represents a bold db transaction and exposes the query and update routines
+// usage:
+//
+// tx, err := db.NewTransaction()
+// defer tx.Close()
+// tx.Get(...)
+// ...
+// tx.Put(...)
+// tx.Commit()
+type Transaction struct {
+	tx         *bolt.Tx
+	isFinished bool
+}
+
+// Commit commits and closes the transaction
+func (tx *Transaction) Commit() error {
+	tx.isFinished = true
+	return tx.tx.Commit()
+}
+
+// Rollback discards all changes and closes the transaction
+func (tx *Transaction) Rollback() error {
+	tx.isFinished = true
+	return tx.tx.Rollback()
+}
+
+// Close closes the transaction. If neither Commit nor Rollback were called before,
+// it rollbacks the transaction
+func (tx *Transaction) Close() error {
+	if !tx.isFinished {
+		return tx.Rollback()
+	}
+	return nil
+}
+
+// Put inserts a doc into a bucket
+func (tx *Transaction) Put(bucketPath, key string, val map[string]interface{}) error {
+	bucket, err := tx.getBucketOrCreate(bucketPath)
+	if err != nil {
+		log.Print("bucket err:", err)
+		return err
+	}
+	bs, err := tx.dataToBytes(val)
+	if err != nil {
+		return err
+	}
+	return bucket.Put([]byte(key), bs)
+}
+
+// Get retrieves a doc from a bucket
+func (tx *Transaction) Get(bucketPath, key string) (map[string]interface{}, error) {
+	bucket, err := tx.getBucket(bucketPath)
+	if err != nil {
+		return nil, err
+	}
+	data := bucket.Get([]byte(key))
+	return tx.bytesToData(data)
+}
+
+// Delete deletes a doc from a bucket
+func (tx *Transaction) Delete(bucketPath, key string) error {
+	bucket, err := tx.getBucket(bucketPath)
+	if err != nil {
+		return err
+	}
+	return bucket.Delete([]byte(key))
+}
+
+// GetBucketContent returns all docs in a bucket
+func (tx *Transaction) GetBucketContent(bucketPath string) (chan *Pair, error) {
+	returnChannel := make(chan *Pair, 64)
+	bucket, err := tx.getBucket(bucketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer close(returnChannel)
+		defer tx.Close()
+		e := bucket.ForEach(func(k, v []byte) error {
+			if v != nil {
+				value, e := tx.bytesToData(v)
+				if e != nil {
+					return e
+				}
+				returnChannel <- &Pair{string(k), value}
+			}
+			return nil
+		})
+		if e != nil {
+			log.Print(e)
+		}
+	}()
+
+	return returnChannel, nil
+}
+
+// GetPrefix returns all docs in a bucket matching a prefix
+func (tx *Transaction) GetPrefix(bucketPath, prefix string) (chan *Pair, error) {
+	returnChannel := make(chan *Pair, 64)
+
+	bucket, err := tx.getBucket(bucketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer close(returnChannel)
+		defer tx.Close()
+		c := bucket.Cursor()
+		for k, v := c.Seek([]byte(prefix)); bytes.HasPrefix(k, []byte(prefix)); k, v = c.Next() {
+			if v != nil {
+				value, e := tx.bytesToData(v)
+				if e != nil {
+					log.Print(e)
+					continue
+				}
+				returnChannel <- &Pair{string(k), value}
+			}
+		}
+	}()
+
+	return returnChannel, nil
+}
+
+// GetRange returns all docs in a bucket matching a prefix
+func (tx *Transaction) GetRange(bucketPath, start, end string) (chan *Pair, error) {
+	returnChannel := make(chan *Pair, 64)
+
+	bucket, err := tx.getBucket(bucketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer close(returnChannel)
+		defer tx.Close()
+		c := bucket.Cursor()
+		for k, v := c.Seek([]byte(start)); k != nil && bytes.Compare(k, []byte(end)) <= 0; k, v = c.Next() {
+			if v != nil {
+				value, e := tx.bytesToData(v)
+				if e != nil {
+					log.Print(e)
+					continue
+				}
+				returnChannel <- &Pair{string(k), value}
+			}
+		}
+	}()
+
+	return returnChannel, nil
+}
+
+// Find searches a bucket for documents
+func (tx *Transaction) Find(bucketPath, filterExpression string) (chan *Pair, error) {
+	stream, err := tx.GetBucketContent(bucketPath)
+	if err != nil {
+		return nil, err
+	}
+	return tx.findFromStream(stream, filterExpression)
+}
+
+// FindPrefix searches a bucket for documents
+func (tx *Transaction) FindPrefix(bucketPath, prefix, filterExpression string) (chan *Pair, error) {
+	stream, err := tx.GetPrefix(bucketPath, prefix)
+	if err != nil {
+		return nil, err
+	}
+	return tx.findFromStream(stream, filterExpression)
+}
+
+// FindRange searches a bucket for documents
+func (tx *Transaction) FindRange(bucketPath, start, end, filterExpression string) (chan *Pair, error) {
+	stream, err := tx.GetRange(bucketPath, start, end)
+	if err != nil {
+		return nil, err
+	}
+	return tx.findFromStream(stream, filterExpression)
+}
+
+func (tx *Transaction) getBucketOrCreate(bucketPath string) (*bolt.Bucket, error) {
+	buckets := strings.Split(bucketPath, ".")
+	bucket, err := tx.tx.CreateBucketIfNotExists([]byte(buckets[0]))
+	if err != nil {
+		return nil, err
+	}
+	if len(bucketPath) > 1 {
+		for _, id := range buckets {
+			bucket, err = bucket.CreateBucketIfNotExists([]byte(id))
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return bucket, nil
+}
+
+func (tx *Transaction) getBucket(bucketPath string) (*bolt.Bucket, error) {
+	buckets := strings.Split(bucketPath, ".")
+	bucket := tx.tx.Bucket([]byte(buckets[0]))
+	if bucket == nil {
+		return nil, errors.New("no such bucket")
+	}
+	if len(bucketPath) > 1 {
+		for _, id := range buckets {
+			bucket = bucket.Bucket([]byte(id))
+			if bucket == nil {
+				return nil, errors.New("no such bucket")
+			}
+		}
+	}
+	return bucket, nil
+}
+
+func (tx *Transaction) dataToBytes(data map[string]interface{}) ([]byte, error) {
+	var buff bytes.Buffer
+	encoder := json.NewEncoder(snappy.NewWriter(&buff))
+	if err := encoder.Encode(data); err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
+
+func (tx *Transaction) bytesToData(data []byte) (map[string]interface{}, error) {
+	buff := bytes.NewBuffer(data)
+	decoder := json.NewDecoder(snappy.NewReader(buff))
+	value := make(map[string]interface{})
+	err := decoder.Decode(&value)
+	if err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+func (tx *Transaction) findFromStream(stream chan *Pair, filterExpression string) (chan *Pair, error) {
+	res := make(chan *Pair, 64)
+	tokens, err := jee.Lexer(filterExpression)
+	if err != nil {
+		return nil, err
+	}
+	tree, err := jee.Parser(tokens)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer close(res)
+		for pair := range stream {
+			if val, err := jee.Eval(tree, pair.Value); err == nil {
+				if match, ok := val.(bool); ok && match {
+					res <- pair
+				}
+			} else {
+				log.Print(err)
+			}
+		}
+	}()
+	return res, nil
+}
